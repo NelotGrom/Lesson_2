4f45d60de1e16af44977137071682e25b5905a46
Nelot <nelotgrom@gmail.com>
2024-01-14 17:12:32 +0100

    feat: short name option added (HW 2.1.4)

--- task_score.js
+++ task_score.js
@@ -3,6 +3,6 @@ let gitMark = 2;
 let jsMark = 3;
 let avgMark = (introMark + gitMark + jsMark) / 3;
 console.log(avgMark);
-let firstName = "Konstantin";
+let firstName = "Konstantin".slice(0,1);
 let lastName = "Kojemiakin";
-console.log(`У товарища ${firstName} ${lastName} средняя оценка ${avgMark}.`);
+console.log(`У товарища ${firstName}.${lastName} средняя оценка ${avgMark}.`);
