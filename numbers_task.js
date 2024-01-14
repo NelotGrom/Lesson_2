39699afd54b3a98fe004ec2ae9c0710916b585d4
Nelot <nelotgrom@gmail.com>
2024-01-14 17:27:39 +0100

    init: create test data gen script (HW 2.1.5)

---
+++ numbers_task.js
@@ -0,0 +1,11 @@
+const value1 = 100;
+let value2 = Math.floor(Math.random() * 25);
+let result = value1 + value2 / 5 + 17;
+
+/*
+1. Генерация рандомного целого числа.
+2. Операция деления.
+3. Сложение.
+*/
+
+console.log(result);
