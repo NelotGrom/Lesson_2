70d9ef8a1a2e3bd47121a0e4d3138966ce3d9e50
unknown <nelotgrom@gmail.com>
2024-01-14 17:02:31 +0100

    init: avg score js file (HW 2.1.3)

---
+++ task_score.js
@@ -0,0 +1,8 @@
+let introMark = 1;
+let gitMark = 2;
+let jsMark = 3;
+let avgMark = (introMark + gitMark + jsMark) / 3;
+console.log(avgMark);
+let firstName = "Konstantin";
+let lastName = "Kojemiakin";
+console.log(`У товарища ${firstName} ${lastName} средняя оценка ${avgMark}.`);
