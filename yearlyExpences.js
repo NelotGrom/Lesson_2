d345465052660cc5a6af48d87b5570d1623517ea
Nelot <nelotgrom@gmail.com>
2024-01-25 16:55:44 +0100

    feat: month reader for <=1k expenses (HW 2.4.2)

--- yearlyExpences.js
+++ yearlyExpences.js
@@ -6,17 +6,22 @@ let expencesExamples = [ // кажется в задании опечатка в
 
 function getExpensesLowerThanK (array) { // расчёт суммы затрат на один массив
     let yearlySum =0;
-    for (i=0; array.length >= i; i++) {
-        if (array[i]>1000) {
+        for (i=0; array.length > i; i++) {
+        if (array[i]>1000) {            //считаем затраты за 1к +
             yearlySum += array[i];
+        } else {                                  //врываемся в затраты меньше либо равные 1000 
+            let date = new Date;            //создаём дата объект
+            date.setMonth(i);           // присваеваем ему значение месяца из индекса текущей итерации
+            console.log(date.toLocaleString('en-US', { month: 'long' }))            //трансформируем в стрингу, отсылаем в консоль
         }
     }
     console.log(`За год ${yearlySum}`); 
 }
 
-console.log(expencesExamples[0].yearlyExpences); 
-getExpensesLowerThanK(expencesExamples[0].yearlyExpences); //вызов функции для подсчёта одного массива
+// console.log(expencesExamples[0].yearlyExpences); 
+// getExpensesLowerThanK(expencesExamples[0].yearlyExpences); //вызов функции для подсчёта одного массива
 
 expencesExamples.forEach(function (eE) { // к каждому элементу expencesExamples(eE) обращаемся с функцией подсчёта
     getExpensesLowerThanK(eE.yearlyExpences); // в аргумент передаём свойство еЕ : массив затрат
+    console.log('=================================================')            // графический консольный разделитель
 });
